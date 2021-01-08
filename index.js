8503eb454e6ca0b9e1ddd911117179a02bf99c31
JohnDJake <john.d.jake@gmail.com>
2021-01-08 14:56:03 -0800

    created an array of questions

--- index.js
+++ index.js
@@ -2,9 +2,48 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("utils/generateMarkdown.js");
+const { title } = require("process");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+}, {
+    type: "editor",
+    name: "desc",
+    message: answers => `Describe ${answers.title}`
+}, {
+    type: "editor",
+    name: "install",
+    message: answers => `How do you install ${answers.title}?`
+}, {
+    type: "editor",
+    name: "usage",
+    message: answers => `How do you use ${answers.title}?`
+}, {
+    type: "editor",
+    name: "contribute",
+    message: answers => `How do you contribute to ${answers.title}?`
+}, {
+    type: "editor",
+    name: "test",
+    message: answers => `How do you test ${answers.title}?`
+}, {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]
+}, {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+}, {
+    type: "input",
+    name: "email",
+    message: "What is the title of your project?",
+    validate: answers => answers.email.includes("@")
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
